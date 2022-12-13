da95bf76e930abd98338648cc2ed1b49846ebafc
LL0YD7 <didintlembele123@gmail.com>
2022-12-13 12:02:40 -0800

    declared statements in main.h file

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - chec the code
  *
  * Return: Always 0.
  */
-
 int main(void)
 
 {
-	print_alphabet();
+	print_alphabet_x10();
 	return (0);
 }
