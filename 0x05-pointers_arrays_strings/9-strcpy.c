0a7aba1c7461f4277ee2b234fee39d3ff0e9cd27
LL0YD7 <didintlembele123@gmail.com>
2022-12-20 08:51:41 -0800

    Fixed coding error

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,14 +8,17 @@
  * @dest: A pointer to destination of string
  * @src: A pointer to source string to copy from
  *
- * Return: Nothing
+ * Return: A pointer to the destination string @dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	char *aux = dest;
+	int index = 0;
 
-	while (*src)
-		*dest++ = *src++;
-	return (aux);
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	return (dest);
 }
