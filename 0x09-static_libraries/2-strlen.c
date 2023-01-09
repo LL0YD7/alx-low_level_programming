0000f75319397c971f9315db1d004b11414c405d
LL0YD7 <didintlembele123@gmail.com>
2023-01-09 05:35:45 -0800

    code

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Write a function that returns the length of a string
+ * @s: An input string
+ * Return: Nothing
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
